4521019a96ad1e7c56d215388140912586a86b73
Александр <sasha@localhost.localdomain>
2023-02-19 12:00:01 +0300

    Вторая задача

--- Zadacha_19/Program.cs
+++ Zadacha_19/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+int chislo = 0;
+int vvedennoeChiclo = 0;
+
+// Проверка на пятизначность:
+while (chislo < 10000 || chislo > 99999)
+{
+    Console.Write("Введи пятизначное число: ");
+    chislo = Convert.ToInt32(Console.ReadLine());
+    vvedennoeChiclo = chislo;
+    if (chislo < 0) chislo = chislo * (-1);
+    if (chislo < 10000 || chislo > 99999) Console.WriteLine("Введено НЕ пятизначное число. Попробуй ещё раз.");
+}
+
+//Вычисление 1,2,4,5-й цифры чила:
+int chislo_1 = chislo / 10000;
+int chislo_2 = chislo / 1000 % 10;
+int chislo_4 = chislo / 10 % 10;
+int chislo_5 = chislo % 10;
+
+if ((chislo_1 == chislo_5) && (chislo_2 == chislo_4))
+{
+    Console.WriteLine($"Введённое число {vvedennoeChiclo} является палиндромом.");
+}
+else
+{
+    Console.WriteLine($"Введённое число {vvedennoeChiclo} НЕ является палиндромом.");
+}
---
+++ Zadacha_21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.Write("Введи для точки A координату X: ");
+int AX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи для точки A координату Y: ");
+int AY = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи для точки A координату Z: ");
+int AZ = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи для точки B координату X: ");
+int BX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи для точки B координату Y: ");
+int BY = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи для точки B координату Z: ");
+int BZ = Convert.ToInt32(Console.ReadLine());
+
+double rasstoyanie = Math.Round(Math.Sqrt(Math.Pow(AX-BX, 2) + Math.Pow(AY-BY, 2) + Math.Pow(AZ-BZ, 2)), 2);
+Console.WriteLine("Расстояние между точками А и В равно " + rasstoyanie);
